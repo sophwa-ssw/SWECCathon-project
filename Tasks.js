c7c65166ff82f30e1bdbae9c0e44d3c261c30a59
Hiya Mehta <140563978+HiyaMehta02@users.noreply.github.com>
2025-04-29 01:22:02 -0700

    Add files via upload

--- Tasks.js
+++ Tasks.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Tasks = () => {
   return (
     <View style={styles.newElement}>
       <Text style={styles.text}>Tasks</Text>
-      <FlatList style={styles.list}
-        data={[
-          {task: '\u2022 Take a picture with a statue'},
-          {task: '\u2022 Take a picture with a goose'},
-          {task: '\u2022 Take a picture with a tree'},
-          {task: '\u2022 Take a picture of a husky '},
-          {task: '\u2022 Take a picture of the famous fruitless trees'},
-          {task: "\u2022 Take a picture of the rock's sights"},
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item.task}</Text>}/>
     </View>
   );
 };
@@ -34,12 +24,6 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 30
-  },
-  list: {
-  alignItems: 'right',
-  flex: 1,
-  },item: {
-    fontSize: 20,
   }
 });
 
